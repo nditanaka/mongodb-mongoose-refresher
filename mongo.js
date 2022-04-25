42a450fbdf2a8d7f0bc06d123f7fe87e0b258a33
nditanaka <tanaching@gmail.com>
2022-04-25 16:32:44 -0400

    add createProduct and getProduct logic

--- mongo.js
+++ mongo.js
@@ -1,29 +1,28 @@
 const MongoClient = require('mongodb').MongoClient
 
-const url = 'mongodb+srv://shonacoder:oUcBmBYNgAxd5qAD@cluster0.5q4gx.mongodb.net/products_test?retryWrites=true&w=majority'
+const url ='mongodb+srv://shonacoder:oUcBmBYNgAxd5qAD@cluster0.5q4gx.mongodb.net/products_test?retryWrites=true&w=majority'
+
+
 const createProduct = async (req, res, next) => {
-    const newProduct = {
-        name: req.body.name,
-        price: req.body.price
-    }
-    const client = new MongoClient(url) // tells mongodb which server we want to connect to
+  const newProduct = {
+    name: req.body.name,
+    price: req.body.price
+  };
+  const client = new MongoClient(url);
 
-    try {
-        await client.connect() // establish connection to server
-        const db = client.db()
-        const result = db.collection('products').insertOne(newProduct)
-    } catch (error) {
-        return res.json({message: 'Could not store data.'})
-    }
-    client.close()
-    
-    res.json(newProduct)
-    
-}
+  try {
+    await client.connect();
+    const db = client.db();
+    const result = db.collection('products').insertOne(newProduct);
+  } catch (error) {
+    return res.json({message: 'Could not store data.'});
+  };
+//   client.close(); // should be included to close the open connection but might not be necessary actually because sockets are pooled??
 
-const getProducts = async (req, res, next) => {
+  res.json(newProduct);
+};
 
-}
+const getProducts = async (req, res, next) => {};
 
-exports.createProduct = createProduct
-exports.getProducts = getProducts
+exports.createProduct = createProduct;
+exports.getProducts = getProducts;
