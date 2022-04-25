3325192931305eebf93d730dc1f2230003bac64d
nditanaka <tanaching@gmail.com>
2022-04-25 19:39:34 -0400

    connect to mongoose instead of MongoClient

--- mongoose.js
+++ mongoose.js
@@ -1,12 +1,25 @@
 const mongoose = require('mongoose')
+require('dotenv').config();
 
 const Product = require('./models/product')
 
+const url =`mongodb+srv://${process.env.USERNAME}:${process.env.PASSWORD}@${process.env.CLUSTER_NAME}.mongodb.net/${process.env.COLLECTION}?retryWrites=true&w=majority`
+
+mongoose.connect(url)
+    .then(() => {
+    console.log('Connected to database!')
+    }).catch(() => {
+    console.log('Connection failed!')
+})
+
+
 const createProduct = async (req, res, next) => {
-    const createProduct = new Product({
+    const createdProduct = new Product({
         name: req.body.name,
-        price: req.body.prince
+        price: req.body.price
     })
+    const result = createdProduct.save()
+    res.json(createdProduct)
 }
 
 exports.createProduct = createProduct
